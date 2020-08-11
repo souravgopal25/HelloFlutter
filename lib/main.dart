dd91567713ad2b8951f4b8fd37758f77a3f32d0e
Souarv Sharma <souravgopal25@gmail.com>
2020-08-11 20:20:40 +0530

    Container & Text

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,31 @@ class HomePage extends StatelessWidget{
         title:  Text("Hello Flutter"),
       ),
       body: Container(
-        child: Center(child: Text("GREAT")),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            width:100,
+            height: 100,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [BoxShadow(
+                  color: Colors.grey[400],
+                  blurRadius: 5,
+                  offset: Offset(2.0,10.0),
+                )],
+                color: Colors.teal,
+                gradient:LinearGradient(colors: [Colors.green,Colors.teal]) ),
+            child: Text("I am a Box",
+            textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        )
       ),
     );
   }
